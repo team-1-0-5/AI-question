2e92c8bc81d825f50cceb3bfb051c70c6f2e5eda
xx666zz <3102531970@qq.com>
2025-07-18 21:16:01 +0800

    new pages

--- vue/src/router/index.ts
+++ vue/src/router/index.ts
@@ -4,6 +4,8 @@ import CreateLecture from '@/pages/CreateLecture.vue';
 import AnswerQuiz from '@/pages/AnswerQuiz.vue';
 import AnswerRes from '@/pages/AnswerRes.vue';
 import Speaker_MainLct from '@/pages/Speaker_MainLct.vue';
+import AudienceHome from '@/pages/AudienceHome.vue';
+import LectureDetail from '@/pages/LectureDetail.vue';
 
 const routes = [
   {
@@ -30,6 +32,41 @@ const routes = [
     path: '/speaker-main-lct',
     name: 'Speaker_MainLct',
     component: Speaker_MainLct
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('@/pages/Login.vue')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: () => import('@/pages/Register.vue')
+  },
+  {
+    path: '/audience-home',
+    name: 'AudienceHome',
+    component: AudienceHome
+  },
+  {
+    path: '/lecture-detail',
+    name: 'LectureDetail',
+    component: LectureDetail
+  },
+  {
+    path: '/audience-lecture-play',
+    name: 'Audience_LecturePlay',
+    component: () => import('@/pages/Audience_LecturePlay.vue')
+  },
+  {
+    path: '/audience-history',
+    name: 'AudienceHistory',
+    component: () => import('@/pages/AudienceHistory.vue')
+  },
+  {
+    path: '/audience-profile',
+    name: 'AudienceProfile',
+    component: () => import('@/pages/AudienceProfile.vue')
   }
 ];
 
