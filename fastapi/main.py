e9eb95a6b7e01260fcf56b5c29666670cd427d9b
pei-xiaobo <2242749482@qq.com>
2025-07-22 19:58:20 +0800

    冲突解决

--- fastapi/main.py
+++ fastapi/main.py
@@ -4,6 +4,7 @@ import time
 import uvicorn
 from fastapi import FastAPI, UploadFile, File, Form, HTTPException
 from fastapi.responses import FileResponse
+from starlette.middleware.cors import CORSMiddleware
 from tortoise.contrib.fastapi import register_tortoise
 from tortoise.exceptions import DoesNotExist
 
@@ -12,8 +13,15 @@ from DAO.models import User, File, UserFile
 from config import DB_CONFIG
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 允许的源
+    allow_credentials=True,
+    allow_methods=["*"],  # 允许所有方法
+    allow_headers=["*"],  # 允许所有请求头
+)
 
-#数据库绑定
+# 数据库绑定
 register_tortoise(
     app,
     config=DB_CONFIG,
